0eb817c917790d3249c9f5bb9f3d3d684e43de61
QA Trinh Le <ltqa1202@g.ucla.edu>
2023-12-05 01:59:35 -0800

    Add project files.

---
+++ ProductivityAlarm/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace ProductivityAlarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.alarm_btn = new System.Windows.Forms.Button();
+            this.currentTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // alarm_btn
+            // 
+            this.alarm_btn.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.alarm_btn.BackgroundImage = global::ProductivityAlarm.Properties.Resources.relaxing_background_aqzjl5if48n6fwf0;
+            this.alarm_btn.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.alarm_btn.Location = new System.Drawing.Point(343, 30);
+            this.alarm_btn.Name = "alarm_btn";
+            this.alarm_btn.Size = new System.Drawing.Size(118, 57);
+            this.alarm_btn.TabIndex = 0;
+            this.alarm_btn.Text = "Set Alarm!";
+            this.alarm_btn.UseVisualStyleBackColor = false;
+            this.alarm_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // currentTime
+            // 
+            this.currentTime.AutoSize = true;
+            this.currentTime.Font = new System.Drawing.Font("Lucida Sans Typewriter", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentTime.Location = new System.Drawing.Point(317, 115);
+            this.currentTime.Name = "currentTime";
+            this.currentTime.Size = new System.Drawing.Size(169, 55);
+            this.currentTime.TabIndex = 1;
+            this.currentTime.Text = "00:00";
+            this.currentTime.Click += new System.EventHandler(this.currentTime_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.currentTime);
+            this.Controls.Add(this.alarm_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button alarm_btn;
+        private System.Windows.Forms.Label currentTime;
+    }
+}
+
---
+++ ProductivityAlarm/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProductivityAlarm
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private NotifyIcon icon;
+        private int elapsedTimeInSeconds;
+        private WorkingStates currentState;
+        private WorkingStates prevState;
+
+        enum WorkingStates
+        {
+            Initializing,
+            Working,
+            Break,
+            Pausing
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+
+            elapsedTimeInSeconds = 0;
+            prevState = WorkingStates.Initializing; 
+            currentState = WorkingStates.Initializing;
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Update the elapsed time
+            elapsedTimeInSeconds++;
+
+            // Show notification every 25 minutes
+            if (elapsedTimeInSeconds % (25 * 60) == 0 && currentState == WorkingStates.Working)
+            {
+                bool userClickOK = ShowPopup("Timer Alert", "It's time to take a break!");
+                timer.Stop();
+                elapsedTimeInSeconds = 0;
+                if (userClickOK)
+                {
+                    timer.Start();
+                    currentState = WorkingStates.Break;
+                }
+            }
+            else if (elapsedTimeInSeconds % (5 * 60) == 0 && currentState == WorkingStates.Break)
+            {
+                bool userClickOK = ShowPopup("Timer Alert", "Break is over!");
+                timer.Stop();  
+                elapsedTimeInSeconds = 0;
+                if (userClickOK)
+                {
+                    timer.Start();
+                    currentState = WorkingStates.Working;
+                }
+            }
+            // Update the timer display on the form
+            UpdateTimerDisplay();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer.Enabled)
+            {
+                timer.Stop();
+                alarm_btn.Text = "Resume";
+                prevState = currentState;
+                currentState = WorkingStates.Pausing;
+            }
+            else if (currentState == WorkingStates.Pausing)
+            {
+                timer.Start();
+                alarm_btn.Text = "Pause";
+                currentState = prevState;
+            }
+            else // first click
+            {
+                timer.Start();
+                alarm_btn.Text = "Pause";
+                currentState = WorkingStates.Working;
+            }
+        }
+
+        private bool ShowPopup(string title, string message)
+        {
+            DialogResult result = MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            return result == DialogResult.OK;
+        }
+
+        private void currentTime_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdateTimerDisplay()
+        {
+            // Update the label or any other control to display the timer
+            int minutes = elapsedTimeInSeconds / 60;
+            int seconds = elapsedTimeInSeconds % 60;
+            currentTime.Text = $"{minutes:D2}:{seconds:D2}";
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Stop the timer and hide the notify icon when the form is closing
+            timer.Stop();
+        }
+    }
+}
